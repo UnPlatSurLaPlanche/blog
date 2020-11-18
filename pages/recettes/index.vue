ff503bc433ff78f88f829d79e891648ae0292e0d
Charles Poullain <charles.poullain85@gmail.com>
2020-11-18 20:57:23 +0100

    feat: display tags present in recipes posts without duplicates

--- pages/recettes/index.vue
+++ pages/recettes/index.vue
@@ -12,9 +12,18 @@
       <h3 class="text-2xl text-secondary">
         Découvrez nos recettes, par catégorie
       </h3>
+      <div class="flex space-x-4 mt-4">
+        <div v-for="tag in tags" :key="tag">
+          <button
+            class="bg-primary bg-opacity-10 text-primary w-full font-semibold py-2 px-3 rounded-lg text-sm focus:outline-none"
+          >
+            {{ tag }}
+          </button>
+        </div>
+      </div>
       <div class="flex flex-wrap w-4/6 mt-10">
         <RecipePreview
-          v-for="post in recettes"
+          v-for="post in recepies"
           :key="post.id"
           :post="post"
           class="w-1/3 my-6"
@@ -27,10 +36,19 @@
 export default {
   name: 'Recettes',
   async asyncData({ $content }) {
-    const recettes = await $content('recettes').fetch()
-    console.log('recettes', recettes)
+    const recepies = await $content('recettes').fetch()
+
+    const recepiesTags = (
+      await $content('recettes').only(['tags']).fetch()
+    ).map((item) => {
+      return item.tags
+    })
+
+    const tags = [...new Set([].concat.apply([], recepiesTags))]
+
     return {
-      recettes,
+      recepies,
+      tags,
     }
   },
 }

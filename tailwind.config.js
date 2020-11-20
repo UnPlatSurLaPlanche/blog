92fc2e2897c1078b219180a82fc98f6b3bad358b
Charles Poullain <charles.poullain85@gmail.com>
2020-11-20 12:15:47 +0100

    feat: update gray colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
@@ -13,7 +11,13 @@ module.exports = {
         primary: '#F0A07C',
         secondary: '#4A274F',
         tertiary: '#B76642',
-        gray: colors.blueGray,
+        gray: {
+          200: '#E7E2F0',
+          400: '#9E94B8',
+          500: '#75648B',
+          800: '#251E3B',
+          900: '#09050A',
+        },
       },
       opacity: {
         10: '0.10',
